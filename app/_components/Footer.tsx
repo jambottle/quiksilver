4048f1bfc9ec26b05c68525c197e37c3e4f1bf53
jambottle <jaewon.walks@gmail.com>
2024-05-05 22:40:03 +0900

    feat: 기본 레이아웃 설정 (Header, Footer)

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="p-2 bg-white">Footer</footer>;
+}
---
+++ app/_components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="p-2 bg-white">Header</header>;
+}
