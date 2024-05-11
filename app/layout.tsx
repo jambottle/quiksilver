a91b6b86de683de98d78df2c46c4b1b77c05d27a
jambottle <jaewon.walks@gmail.com>
2024-05-11 14:35:20 +0900

    feat: TansStack Query, TansStack Query Devtools 설치
    - 참고: https://tanstack.com/query/latest/docs/framework/react/guides/advanced-ssr#initial-setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import '@/globals.css';
 
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Analytics } from '@vercel/analytics/react';
 
 import Footer from '@/_components/Footer';
 import Header from '@/_components/Header';
 import { pretendard } from '@/fonts';
+import ReactQueryProvider from '@/provider';
 
 export default function RootLayout({
   children,
@@ -12,13 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={pretendard.variable}>
-      <body>
-        <Header />
-        <main className="quiksilver-layout">{children}</main>
-        <Footer />
-        <Analytics />
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="ko" className={pretendard.variable}>
+        <body>
+          <Header />
+          <main className="quiksilver-layout">{children}</main>
+          <Footer />
+          <Analytics />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </body>
+      </html>
+    </ReactQueryProvider>
   );
 }
